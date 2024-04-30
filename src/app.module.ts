61defbdaa4384c6beea04497a803e746ae7d266f
Samiyya24 <samiyyaazizkhanova904@gmail.com>
2024-04-30 08:50:01 +0500

    added CRUD table

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { ObjectTypeModule } from './object-type/object-type.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
     PrismaModule,
+    ObjectTypeModule,
   ],
   controllers: [],
   providers: [],
